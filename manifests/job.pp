6cdb35db5f427f85b4061de2013419afae24d801
Lee Briggs <lbriggs@apptio.com>
2016-11-21 19:21:11 +0000

    Add support for sensu_wrapper

--- manifests/job.pp
+++ manifests/job.pp
@@ -12,7 +12,23 @@ define cron::job (
   $user = 'root',
   $environment = [],
   $lock = false,
+  $monitor_job = undef,
+  $monitor_ttl = undef,
+  $monitor_source = undef,
+  $monitor_timeout = undef,
 ) {
+
+  if $monitor_job {
+    validate_bool($monitor_job)
+
+    include ::sensu_wrapper
+    if $monitor_ttl {
+      validate_integer($monitor_ttl)
+    }
+    if $monitor_timeout {
+      validate_integer($monitor_timeout)
+    }
+  }
 
   file { "/etc/cron.d/job_${title}":
     ensure  => $ensure,
