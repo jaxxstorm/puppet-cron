285a5d5c4eba197a5c6b2012c83a02f19c867bef
Lee Briggs <lbriggs@apptio.com>
2016-11-27 15:28:15 +0000

    Add params for job
    And update gitignore file

--- manifests/job.pp
+++ manifests/job.pp
@@ -1,6 +1,25 @@
 # Define: cron::job
-# 
 # Generate a cron job in /etc/cron.d
+#
+# @example Create a cron job
+#   ::cron::job { 'my_job':
+#     command  => '/bin/echo "hello"',
+#   }
+#
+# @param [String] command Specifies the command to run in the job (required)
+# @param [String] ensure Specifies whether the job should exist or not
+# @param [String] minute Specifies the cron style minute the job should run
+# @param [String] hour Specifies the cron style hour the should run
+# @param [String] day_of_month Specifies the cron style day of the month the job should run
+# @param [String] month Specifies the cron style month the job should run
+# @param [String] day_of_week Specifies the cron style day of the week the job should run
+# @param [String] user Specifies the user the cron job should run as
+# @param [String] environment Specifies an array of environment variables for the job in fhe format `ENV=value`
+# @param [String] lock Specifies whether the cron job should lock itself using `flock`
+# @param [String] monitor_job Specifies whether you want to monitor the job using sensu-wrapper
+# @param [String] monitor_ttl Specifies a TTL for the job for sensu-wrapper
+# @param [String] monitor_source Specifies a TTL for the job for sensu-wrapper
+# @param [String] monitor_timeout Specifies a timeout for sensu-wrapper before the job should halt and fail
 define cron::job (
   $command,
   $ensure = 'present',
